674bac6aea4742351e80042baa7d10ddf6d784aa
Arun S Jois <arunsjois@outlook.com>
2017-05-05 12:16:09 +0530

    LICENSE

--- ip.php
+++ ip.php
@@ -1,6 +1,6 @@
 <?php
-//$command="/sbin/ifconfig wlp2s0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'";
-$command="/sbin/ifconfig enp3s0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'";
+$command="/sbin/ifconfig wlp2s0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'";
+//$command="/sbin/ifconfig enp3s0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'";
 $localIP = exec ($command);
 //$localIP="192.168.43.245";
 //$localIP="192.168.1.105";
